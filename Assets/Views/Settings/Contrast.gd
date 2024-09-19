f9d84b436b53231d59812e5258917ab562b3476e
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-09-19 20:30:06 +0000

    full refactoring

--- Assets/Views/Settings/Contrast.gd
+++
@@ -1,9 +0,0 @@
-extends HBoxContainer
-
-
-func _ready() -> void:
-	$Option.value = Defaults.ui_theme.contrast
-
-
-func _on_CatTheme_values_updated() -> void:
-	$Option.value = Defaults.ui_theme.contrast
--- StartDialog.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	if OS.get_name() == "X11" or OS.get_name() == "Server":
-		get_window().borderless = (false)
-	else:
-		get_window().borderless = (true)
-	get_tree().call_deferred('change_scene_to_file', "res://Main.tscn")

d8ff8e5eb5a28fdf9fc3872d138a4cefd4b3f855
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-09-24 23:45:06 +0000

    script

---
+++ Components/TimeTrackingPanel/NormalTimeTrackingPanel.gd
@@ -0,0 +1,59 @@
+extends Control
+
+@onready var start_button = %StartButton
+@onready var pause_button = %PauseButton
+@onready var finish_button = %FinishButton
+@onready var cancel_button = %CancelButton
+@onready var item_input = %ItemInput
+
+func _ready() -> void:
+	init_component()
+	pass
+
+func _process(_delta: float) -> void:
+	pass
+
+func init_component() :
+	start_button.visible = true
+	pause_button.visible = false
+	finish_button.visible = false
+	cancel_button.visible = false
+	item_input.visible = true
+
+func start_timer() :
+	pause_button.text = 'Pause'
+	start_button.visible = false
+	pause_button.visible = true
+	finish_button.visible = true
+	cancel_button.visible = true
+	pass
+
+func pause_timer() :
+	pause_button.text = 'Continue'
+
+func finish_timer() :
+	pause_button.text = 'Pause'
+	start_button.visible = true
+	pause_button.visible = false
+	finish_button.visible = false
+	cancel_button.visible = false
+
+func cancel_timer() :
+	pause_button.text = 'Pause'
+	start_button.visible = true
+	pause_button.visible = false
+	finish_button.visible = false
+	cancel_button.visible = false
+
+
+func _on_start_button_pressed() -> void:
+	pass
+
+func _on_pause_button_pressed() -> void:
+	pass
+
+func _on_finish_button_pressed() -> void:
+	pass
+
+func _on_cancel_button_pressed() -> void:
+	pass
